2c53c5ec58cdeaad6d1027d7cafa94374c3953da
SoulMourne <goodwinjeanpaul@yahoo.fr>
2016-03-09 09:19:27 +0100

    Initial commit

---
+++ app/src/main/java/com/firstapp/android/whacamole/WhacAMole.java
@@ -0,0 +1,13 @@
+package com.firstapp.android.whacamole;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WhacAMole extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_whac_amole);
+    }
+}
